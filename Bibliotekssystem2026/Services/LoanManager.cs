2cbe523a60f965cf954d28af84114fa37d870f5f
jelal <qaiumi@hotmail.com>
2026-03-01 09:52:15 +0100

    feat: implement LoanManager for handling loans

---
+++ Bibliotekssystem2026/Services/LoanManager.cs
@@ -0,0 +1,19 @@
+﻿using Bibliotekssystem2026.Models;
+
+namespace Bibliotekssystem2026.Services
+{
+    public class LoanManager
+    {
+        private readonly List<Loan> _loans = new();
+
+        public Loan CreateLoan(Book book, Member member, int days = 14)
+        {
+            var loan = new Loan(book, member, DateTime.Now, DateTime.Now.AddDays(days));
+            _loans.Add(loan);
+            return loan;
+        }
+
+        public IEnumerable<Loan> GetActiveLoans() =>
+            _loans.Where(l => !l.IsReturned);
+    }
+}
