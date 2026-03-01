b38e7f7910749430827775c8f2e8766416132cc6
jelal <qaiumi@hotmail.com>
2026-03-01 09:54:40 +0100

    feat: implement Library class using composition

---
+++ Bibliotekssystem2026/Library.cs
@@ -0,0 +1,11 @@
+﻿using Bibliotekssystem2026.Services;
+
+namespace Bibliotekssystem2026
+{
+    public class Library
+    {
+        public BookCatalog BookCatalog { get; } = new();
+        public MemberRegistry MemberRegistry { get; } = new();
+        public LoanManager LoanManager { get; } = new();
+    }
+}
